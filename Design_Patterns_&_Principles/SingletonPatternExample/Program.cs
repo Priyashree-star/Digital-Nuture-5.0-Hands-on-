9da9e854e0848c23b76843da67d392961f48541e
Priyashree-star <priyashreebanerjee2105@gmail.com>
2026-07-05 11:16:53 +0530

    design pattern first exercise

---
+++ Design_Patterns_&_Principles/SingletonPatternExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using SingletonPatternExample.Models;
+
+Logger logger1 = Logger.GetInstance();
+logger1.Log("Application Started");
+
+Logger logger2 = Logger.GetInstance();
+logger2.Log("User Logged In");
+
+Console.WriteLine();
+
+Console.WriteLine(logger1 == logger2);
---
+++ Design_Patterns_&_Principles/SingletonPatternExample/models/Logger.cs
@@ -0,0 +1,26 @@
+namespace SingletonPatternExample.Models;
+
+public class Logger
+{
+    private static Logger? instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger Created");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("[LOG] " + message);
+    }
+}
